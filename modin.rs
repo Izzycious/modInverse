fc4307a861ebb98c37bae3b0fad8620e4933b700
Ishaq Nasir <ishaqnasir@gmail.com>
2021-11-05 16:46:00 -0500

    con

--- modin.rs
+++ modin.rs
@@ -1,18 +1,26 @@
-fn mod_inverse(a: isize, module: isize) -> isize {
-  let mut mn = (module, a);
-  let mut xy = (0, 1);
+fn egcd(mut x: isize, mut y:isize) -> (isize,isize,isize) {
+  let (mut a0,mut a1,mut b0,mut b1) = (1,0,0,1);
 
-  while mn.1 != 0 {
-    xy = (xy.1, xy.0 - (mn.0/mn.1) * xy.1);
-    mn = (mn.1, mn.0 % mn.1);
-  }
+  while y != 0 {
+      // dbg!(x,y);
+      let (q,r)  = (x / y, x % y);
+      let (c, d) = ( a0 - q * a1, b0 - q * b1  ); 
 
-  while xy.0 < 0 {
-    xy.0 += module;
+      x = y;
+      y = r;
+      a0 = a1;
+      a1 = c;
+      b0 = b1; 
+      b1 = d;
+
   }
-  xy.0
+  (x, a0, b0)
 }
 
 fn main() {
-  println!("{}", mod_inverse(57, 2004))
+  let (gcd, a, b) = egcd(180,150);
+  println!("GCD: {}  {} and {}",
+  gcd, a, b);
+  
+  //
 }
