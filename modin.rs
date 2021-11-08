f7f50f03a81e6c69b42bf10d37368acbaf5961b9
Ishaq Nasir <ishaqnasir@gmail.com>
2021-11-08 07:51:23 -0500

    mod

--- modin.rs
+++ modin.rs
@@ -1,5 +1,14 @@
+// Function declaration takes in two parameters of type isize (The pointer-sized signed integer type.) 
+
+//The size of this primitive is how many bytes it takes to reference any location in memory
+// a represents an integer whose multiplicative inverse is to be computed.
+// modulus represents an integer whose value is greater than 1
 fn egcd(mut x: isize, mut y:isize) -> (isize,isize,isize) {
+
+  // Declaring a mutable variable 
   let (mut a0,mut a1,mut b0,mut b1) = (1,0,0,1);
+
+  //loop function allows the execution of code as long as the codition is true usin
 
   while y != 0 {
       // dbg!(x,y);
@@ -17,10 +26,15 @@ fn egcd(mut x: isize, mut y:isize) -> (isize,isize,isize) {
   (x, a0, b0)
 }
 
+    // Rust programs start with fn main()
+    // You put the code inside a block. It starts with { and ends with }
+
 fn main() {
   let (gcd, a, b) = egcd(180,150);
+
+  // Print function in getting the extended euclidean algorithm output of 
   println!("GCD: {}  {} and {}",
   gcd, a, b);
   
-  //
+  //30   1 and -1
 }
